14086c1c9df14e6fab60b3aa0a366a75d67cadbc
iHSPA <harrsh.hp@live.in>
2019-09-20 14:48:10 +0530

    App-Log::Init-Project

---
+++ src/main/java/com/app/rc/gettingStartedWithSpringBootAuth/GettingStartedWithSpringBootAuthApplication.java
@@ -0,0 +1,13 @@
+package com.app.rc.gettingStartedWithSpringBootAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithSpringBootAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedWithSpringBootAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/rc/gettingStartedWithSpringBootAuth/GettingStartedWithSpringBootAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.app.rc.gettingStartedWithSpringBootAuth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GettingStartedWithSpringBootAuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
